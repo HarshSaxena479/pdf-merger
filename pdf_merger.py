1decaeb3b24e80936d87b03e0e1d0ac089f55491
HARSH SAXENA <143206641+HarshSaxena479@users.noreply.github.com>
2026-05-06 11:15:50 +0530

    Delete pdf_merger.py

--- pdf_merger.py
+++
@@ -1,8 +0,0 @@
-from pypdf import PdfWriter
-
-merger = PdfWriter()
-
-for pdf in ["questions.pdf", "OOPS Important Interview Questions.pdf"]:
-    merger.append(pdf)
-
-merger.write("merged_pdf.pdf")
